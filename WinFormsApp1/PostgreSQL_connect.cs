bc972775b89cf858907a0c9fff3442abe6547f1f
diniskss <106401449+diniskss@users.noreply.github.com>
2024-04-30 23:40:54 +0700

    Add classes

---
+++ WinFormsApp1/PostgreSQL_connect.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using WinFormsApp1;
+
+namespace WinFormsApp1
+{
+    internal class PostrgreSQL
+    {
+        private static string Host = "localhost";
+        private static string User = "postgres";
+        private static string DBname = "postgres";
+        private static string Password = "root";
+        private static string Port = "5432";
+        public static void PostgreSQL_Connection()
+        {
+            try
+            {
+                string connString =
+                String.Format(
+                    "Server={0};Username={1};Database={2};Port={3};Password={4};SSLMode=Prefer",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+
+                var conn = new NpgsqlConnection(connString);
+
+            }
+            catch (Exception){
+                Form1.Label1 lb1 = new 
+            }
+        }
+    }
+}
---
+++ WinFormsApp1/sellers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class sellers
+    {
+        public int User_id {  get; set; }
+        public int Rating { get; set; }
+        public string Phone_number {  get; set; }
+        public int Adv_id { get; set; }
+        public sellers(int user_id, int rating, string phone_number, int adv_id)
+        {
+            User_id = user_id;
+            Rating = rating;
+            Phone_number = phone_number;
+            Adv_id = adv_id;
+        }
+    }
+}
